49b62fa6eb2ab0e00d5dbf63355e83bb982dd43d
Stewart McCoy <mccoy.stewart@gmail.com>
2013-03-19 12:16:02 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FootySubs::Application.config.secret_token = '6b9bea2c1e5ac88b9cfd5b12f11730188168fe20cbfb406895f443e363d31ee4b9dd99a08771330229e66a0451d8f14b08866025db52235f595af5e139b5dddd'
