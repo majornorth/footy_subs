1abd1cf830db5e1b2b49bfeeb41de4c3fc346a5d
Stewart McCoy <stewart.mccoy@causes.com>
2013-10-20 13:14:03 -0700

    Disable initialize on precompile of assets to prevent rake failure during deployment. Hope this works.

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module Pickup
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # While precompiling assets, prevent initializing application and connecting to database
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.before_configuration do
